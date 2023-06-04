8975d7843f1c88a805d830c2adadb84127ea0d12
Tatadev13 <marlene.legaspim@gmail.com>
2023-06-04 02:02:57 -0500

    giphy party

---
+++ GiphyP.js
@@ -0,0 +1,40 @@
+let APIKEY = "hhcC88yeu3C73XJoxgOQbuNoaAkYEKWo";
+document.addEventListener("DOMContentLoaded",init);
+function init() {
+    document.getElementById("btnSearch").addEventListener("click", ev=> {
+        ev.preventDefault();
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+        let str = document.getElementById("search").value.trim();
+        url = url.concat(str);
+        console.log(url);
+        fetch(url)
+        .then(response => response.json())
+        .then(content => {
+         let fig = document.createElement("figure");
+         let img = document.createElement ("img");
+         let fc = document.createElement("figcaption");
+         img.src = content.data[0].images.downsized.url;
+         img.alt = content.data[0].title;
+         fc.textContent = content.data[0].title;
+         fig.appendChild(img);
+         fig.appendChild(fc);
+         let out = document.querySelector(".out");
+         out.insertAdjacentElement("afterbegin",fig);
+         document.querySelector("#search").value = "";   
+        })
+        .catch(err => {
+            console.error(err);
+        });
+        
+    });
+}
+
+// remove.addEventListener("click", () => {
+//     // if (container.childElementCount > 0) {
+//       container.removeChild(container.lastElementChild);
+//     // }
+//     // if (number > 1) {
+//     //   number--;
+//     // }
+// });
+
